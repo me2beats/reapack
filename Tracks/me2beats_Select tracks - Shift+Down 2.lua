6543940caa90942bcbe7aaa6b6424871f512a0bc
me2beats <prod.me2beats@gmail.com>
2017-03-20 22:25:16 +0500

    +

---
+++ Tracks/me2beats_Select tracks - Shift+Down 2.lua
@@ -0,0 +1,58 @@
+-- @description Select tracks - Shift+Down 2
+-- @version 1.0
+-- @author me2beats
+-- @changelog
+--  + init
+
+local r = reaper
+
+function set_last_touched(tr)
+
+  local function UnselectAllTracks()
+    local first_track = r.GetTrack(0, 0)
+    r.SetOnlyTrackSelected(first_track)
+    r.SetTrackSelected(first_track, 0)
+  end
+
+  local function SaveSelTracks()
+    sel_tracks = {}
+    for i = 0, r.CountSelectedTracks()-1 do
+      sel_tracks[i+1] = r.GetSelectedTrack(0, i)
+    end
+  end
+
+  local function RestoreSelTracks()
+    UnselectAllTracks()
+    for _, track in ipairs(sel_tracks) do
+      r.SetTrackSelected(track, 1)
+    end
+  end
+
+
+  SaveSelTracks()
+  r.SetOnlyTrackSelected(tr,1)
+  r.Main_OnCommand(40914,0) -- Track: Set first selected track as last touched track
+  RestoreSelTracks()
+end
+
+local last_touched = r.GetLastTouchedTrack()
+local last_touched_num = r.GetMediaTrackInfo_Value(last_touched, 'IP_TRACKNUMBER') 
+
+local first_sel = r.GetSelectedTrack(0,0)
+local last_sel = r.GetSelectedTrack(0,r.CountSelectedTracks()-1)
+
+r.Undo_BeginBlock(); r.PreventUIRefresh(1)
+
+if first_sel == last_sel then
+  r.Main_OnCommand(40287,0) -- Track: Go to next track (leaving other tracks selected)
+else
+  if first_sel == last_touched then
+    r.SetTrackSelected(first_sel,0)
+    local first_sel_num = r.GetMediaTrackInfo_Value(first_sel, 'IP_TRACKNUMBER')
+    set_last_touched(r.GetTrack(0,first_sel_num))
+  elseif last_sel == last_touched then
+    r.Main_OnCommand(40287,0) -- Track: Go to next track (leaving other tracks selected)
+  end
+end
+
+r.PreventUIRefresh(-1); r.Undo_EndBlock('Select tracks', -1)
