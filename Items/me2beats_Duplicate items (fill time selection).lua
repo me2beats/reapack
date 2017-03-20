546a17e11583c67d379a6a3c315324c0ceabfea6
me2beats <prod.me2beats@gmail.com>
2017-03-21 02:05:39 +0500

    +

--- Items/me2beats_Duplicate items (fill time selection).lua
+++ Items/me2beats_Duplicate items (fill time selection).lua
@@ -1,33 +1,56 @@
 -- @description Duplicate items (fill time selection)
--- @version 1.0
+-- @version 1.1
 -- @author me2beats
 -- @changelog
 --  + init
+--  + Several items duplication
 
 local r = reaper; local function nothing() end; local function bla() r.defer(nothing) end
 
-x,y = r.GetSet_LoopTimeRange(0, 0, 0, 0, 0)
+function delete_sel_items_after_time(time)
+
+  local x,y = r.GetSet_LoopTimeRange(0, 0, 0, 0, 0)
+
+  local last_sel = r.GetSelectedMediaItem(0,r.CountSelectedMediaItems()-1)
+  local last_sel_end = r.GetMediaItemInfo_Value(last_sel, 'D_POSITION') + r.GetMediaItemInfo_Value(last_sel, 'D_LENGTH')
+
+  r.GetSet_LoopTimeRange(1, 0, time, last_sel_end+1, 0)
+  r.Main_OnCommand(40061,0)--Item: Split items at time selection
+
+  local items = r.CountSelectedMediaItems()
+  for i = items-1, 0, -1 do
+    local it = r.GetSelectedMediaItem(0,i)
+    local it_pos = r.GetMediaItemInfo_Value(it, 'D_POSITION')
+    if time-it_pos > 0.00001 then r.SetMediaItemSelected(it,0) end
+  end
+
+  r.Main_OnCommand(40006,0)--Item: Remove items
+
+  r.GetSet_LoopTimeRange(1, 0, x, y, 0)
+end
+
+local x,y = r.GetSet_LoopTimeRange(0, 0, 0, 0, 0)
 if x == y then bla() return end
 
-item = r.GetSelectedMediaItem(0,0)
-if not item then bla() return end
+local first_sel = r.GetSelectedMediaItem(0,0)
+if not first_sel then bla() return end
 
-it_len = r.GetMediaItemInfo_Value(item, 'D_LENGTH')
+local cur = r.GetCursorPosition()
+
+local last_sel = r.GetSelectedMediaItem(0,r.CountSelectedMediaItems()-1)
+local first_sel_pos = r.GetMediaItemInfo_Value(first_sel, 'D_POSITION')
+local last_sel_end = r.GetMediaItemInfo_Value(last_sel, 'D_POSITION') + r.GetMediaItemInfo_Value(last_sel, 'D_LENGTH')
+local len = last_sel_end-first_sel_pos
 
 r.Undo_BeginBlock() r.PreventUIRefresh(1)
 
-r.SetMediaItemInfo_Value(item, 'D_POSITION',x)
-copies = math.floor((y-x)/it_len)
-r.ApplyNudge(0, 0, 5, 20, 1, 0, copies)
-item = r.GetSelectedMediaItem(0,0)
-it_start = r.GetMediaItemInfo_Value(item, 'D_POSITION')
-
-if it_start >= y-0.0000001 then
-  tr = r.GetMediaItem_Track(item)
-  del = r.DeleteTrackMediaItem(tr, item)
-else
-  r.SetMediaItemInfo_Value(item, 'D_LENGTH',y-it_start)
-  r.SetMediaItemSelected(item,0)
+local ret = r.ApplyNudge(0, 0, 0, 1, x-first_sel_pos, 0, 0)
+for i = 1, math.floor((y-x)/len) do
+  r.Main_OnCommand(41295,0)--Item: Duplicate items
 end
 
-r.PreventUIRefresh(-1) r.Undo_EndBlock('Duplicate items (fill time selection)', -1)
+delete_sel_items_after_time(y)
+
+r.SetEditCurPos2(0, cur, 0, 0)
+
+r.PreventUIRefresh(-1) r.Undo_EndBlock('Duplicate items', -1)
