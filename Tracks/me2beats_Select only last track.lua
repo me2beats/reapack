3a96774703bf616eb76ba138d18d9675d72aa36c
me2beats <prod.me2beats@gmail.com>
2017-03-25 14:42:08 +0500

    +

--- Tracks/me2beats_Select only last track.lua
+++ Tracks/me2beats_Select only last track.lua
@@ -1,18 +1,14 @@
 -- @description Select only last track
--- @version 1.0
+-- @version 1.01
 -- @author me2beats
 -- @changelog
 --  + init
 
-n = reaper.CountTracks(0)
-if n>0 then
-  script_title = "select only last track"
-  reaper.Undo_BeginBlock()
-  tr = reaper.GetTrack(0, n-1)
-  reaper.SetOnlyTrackSelected(tr)
-  reaper.Undo_EndBlock(script_title, -1)
-else
-  function nothing()
-  end
-  reaper.defer(nothing)
-end
+local r = reaper; local function nothing() end; local function bla() r.defer(nothing) end
+
+n = r.CountTracks()
+if n == 0 then bla() return end
+
+r.Undo_BeginBlock()
+r.SetOnlyTrackSelected(r.GetTrack(0,n-1))
+r.Undo_EndBlock('Select only last track', -1)
