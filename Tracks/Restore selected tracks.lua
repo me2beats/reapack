c0c1f5f84bb699e2743f5f672e4cf137adab8143
me2beats <prod.me2beats@gmail.com>
2017-03-25 13:40:05 +0500

    +

---
+++ Tracks/Restore selected tracks.lua
@@ -0,0 +1,25 @@
+local r = reaper; local function nothing() end; local function bla() r.defer(nothing) end
+
+local function UnselectAllTracks()
+  local first_track = r.GetTrack(0, 0)
+  r.SetOnlyTrackSelected(first_track)
+  r.SetTrackSelected(first_track, 0)
+end
+
+local sel_tracks_str = r.GetExtState('me2beats_save-restore', 'sel_tracks')
+if not sel_tracks_str or sel_tracks_str == '' then bla() return end
+
+
+r.Undo_BeginBlock()
+
+r.PreventUIRefresh(1)
+
+UnselectAllTracks()
+for guid in sel_tracks_str:gmatch'{.-}' do
+  tr = r.BR_GetMediaTrackByGUID(0, guid)
+  if tr then r.SetTrackSelected(tr,1) end
+end
+
+r.PreventUIRefresh(-1)
+
+r.Undo_EndBlock('Restore selected tracks', -1)
---
+++ Tracks/Save selected tracks.lua
@@ -0,0 +1,9 @@
+local r = reaper
+
+sel_tracks_str = ''
+for i = 0, r.CountSelectedTracks()-1 do
+  sel_tracks_str = sel_tracks_str..r.GetTrackGUID(r.GetSelectedTrack(0,i))
+end
+
+r.DeleteExtState('me2beats_save-restore', 'sel_tracks', 0)
+r.SetExtState('me2beats_save-restore', 'sel_tracks', sel_tracks_str, 0)
