ea67aab50876c46f0db071cde52aa92a9dbd8611
me2beats <prod.me2beats@gmail.com>
2017-03-26 17:27:33 +0500

    +

--- MIDI Editor/me2beats_Select only note under mouse.lua
+++ MIDI Editor/me2beats_Select only note under mouse.lua
@@ -1,32 +1,35 @@
 -- @description Select only note under mouse
--- @version 1.0
+-- @version 1.01
 -- @author me2beats
 -- @changelog
 --  + init
 
 local r = reaper; local function nothing() end; local function bla() r.defer(nothing) end
 
-take = r.MIDIEditor_GetTake(r.MIDIEditor_GetActive())
+local take = r.MIDIEditor_GetTake(r.MIDIEditor_GetActive())
 if not take then bla() return end
-notes = r.MIDI_CountEvts(take)
-window, segment, details = r.BR_GetMouseCursorContext()
-_, noteRow = r.BR_GetMouseCursorContext_MIDI()
+local notes = r.MIDI_CountEvts(take)
+local window, segment, details = r.BR_GetMouseCursorContext()
+local _,_,noteRow = r.BR_GetMouseCursorContext_MIDI()
+
 if noteRow == -1 then bla() return end
 
-mouse_time = r.BR_GetMouseCursorContext_Position()
-mouse_ppq_pos = r.MIDI_GetPPQPosFromProjTime(take, mouse_time)
+local mouse_time = r.BR_GetMouseCursorContext_Position()
+local mouse_ppq_pos = r.MIDI_GetPPQPosFromProjTime(take, mouse_time)
+
 
 r.Undo_BeginBlock() r.PreventUIRefresh(1)
 
 for i = 0, notes - 1 do
-  _, sel, muted, start_note, end_note, chan, pitch, vel = r.MIDI_GetNote(take, i)
+  local _, sel, muted, start_note, end_note, chan, pitch, vel = r.MIDI_GetNote(take, i)
   if start_note < mouse_ppq_pos and end_note > mouse_ppq_pos and noteRow == pitch then
-    if sel == false then
-      r.MIDI_SetNote(take, i, 1, muted, start_note, end_note, chan, pitch, vel)
-    end
-  elseif sel == true then
-    r.MIDI_SetNote(take, i, 0, muted, start_note, end_note, chan, pitch, vel)
+    note = i
+    break
   end
 end
 
-r.PreventUIRefresh(-1) r.Undo_EndBlock('Select only note under mouse', 2)
+if not note then bla() return end
+r.MIDIEditor_LastFocused_OnCommand(40214, 0)
+r.MIDI_SetNote(take, note, 1, nil, nil, nil, nil, nil, nil)
+
+r.PreventUIRefresh(-1) r.Undo_EndBlock('Select only note under mouse', -1)
