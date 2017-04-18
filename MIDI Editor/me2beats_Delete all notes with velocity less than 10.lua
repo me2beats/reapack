a78f9505c7f96c1e36b1fac0b01b4cc84c683925
me2beats <prod.me2beats@gmail.com>
2017-04-18 18:46:55 +0500

    +

---
+++ MIDI Editor/me2beats_Delete all notes with velocity less than 10.lua
@@ -0,0 +1,27 @@
+-- @description Delete all notes with velocity less than 10
+-- @version 1.0
+-- @author me2beats
+-- @changelog
+--  + init
+
+local r = reaper; local function nothing() end; local function bla() r.defer(nothing) end
+
+local vel_del = 10
+
+local take = r.MIDIEditor_GetTake(r.MIDIEditor_GetActive())
+if not take then bla() return end
+
+local _, notes = r.MIDI_CountEvts(take)
+if notes == 0 then bla() return end
+
+vel_del = tonumber(vel_del)
+if not vel_del or vel_del < 1 then bla() return end
+
+r.Undo_BeginBlock() r.PreventUIRefresh(1)
+
+for i = notes-1, 0, -1 do
+  local _,_,_,_,_,_,_, vel = r.MIDI_GetNote(take, i)
+  if vel < vel_del then r.MIDI_DeleteNote(take, i) end
+end
+
+r.PreventUIRefresh(-1) r.Undo_EndBlock('Delete notes', -1)
