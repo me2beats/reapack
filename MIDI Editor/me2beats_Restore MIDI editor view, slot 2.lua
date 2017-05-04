630d79474f87fc141dead00665719c613d663603
me2beats <prod.me2beats@gmail.com>
2017-05-04 19:16:14 +0500

    +

---
+++ MIDI Editor/me2beats_Restore MIDI editor view, slot 2.lua
@@ -0,0 +1,52 @@
+-- @description Restore MIDI editor view, slot 2
+-- @version 1.0
+-- @author me2beats
+-- @changelog
+--  + init
+
+local r = reaper; local function nothing() end; local function bla() r.defer(nothing) end
+function esc(str) str = str:gsub('%-', '%%-') return str end
+
+local take = r.MIDIEditor_GetTake(r.MIDIEditor_GetActive())
+if not take then bla() return end
+
+local ext_sec, ext_key = 'me2beats_save-restore', 'MIDI view2'
+local view = r.GetExtState(ext_sec, ext_key)
+if not view or view == '' then bla() return end
+
+local guid = r.BR_GetMediaItemTakeGUID(take)
+local item = r.GetMediaItemTake_Item(take)
+
+local tr = r.GetMediaItem_Track(item)
+
+r.Undo_BeginBlock() r.PreventUIRefresh(1)
+
+local sync
+if r.GetToggleCommandStateEx(32060, 40640) == 1 then
+  r.MIDIEditor_LastFocused_OnCommand(40640,0)--Timebase: Toggle sync to arrange view
+  sync = 1
+end
+
+r.SelectAllMediaItems(0, 0); r.SetMediaItemSelected(item, 1)
+
+local _, chunk = r.GetTrackStateChunk(tr, '', 0)
+local a, old_view, b = chunk:match('(.*'..esc(guid)..'.-CFGEDITVIEW)(.-)(\n.*)')
+local new_chunk = a..view..b
+
+r.MIDIEditor_LastFocused_OnCommand(2, 0) -- close editor
+
+local tr_items = r.CountTrackMediaItems(tr)
+for i = tr_items-1,0,-1 do
+  local tr_item = r.GetTrackMediaItem(tr, i)
+  r.DeleteTrackMediaItem(tr, tr_item)
+  if item == tr_item then break end
+end
+
+r.SetTrackStateChunk(tr, new_chunk, 0)
+
+r.Main_OnCommand(40109,0)--Item: Open items in primary external editor
+
+if sync then r.MIDIEditor_LastFocused_OnCommand(40640,0) end --Timebase: Toggle sync to arrange view
+
+r.PreventUIRefresh(-1) r.Undo_EndBlock('Restore MIDI editor view', 2)
+
---
+++ MIDI Editor/me2beats_Save MIDI editor view.lua
@@ -0,0 +1,34 @@
+-- @description Save MIDI editor view
+-- @version 1.0
+-- @author me2beats
+-- @changelog
+--  + init
+
+local r = reaper; local function nothing() end; local function bla() r.defer(nothing) end
+
+function esc(str) str = str:gsub('%-', '%%-') return str end
+
+local take = r.MIDIEditor_GetTake(r.MIDIEditor_GetActive())
+if not take then bla() return end
+
+local guid = r.BR_GetMediaItemTakeGUID(take)
+
+local item = r.GetMediaItemTake_Item(take)
+
+local tr = r.GetMediaItem_Track(item)
+
+local _, chunk = r.GetTrackStateChunk(tr, '', 0)
+
+local view = chunk:match(esc(guid)..'.-CFGEDITVIEW(.-)\n')
+
+if not view then bla() return end
+
+local ext_sec, ext_key = 'me2beats_save-restore', 'MIDI view'
+
+r.DeleteExtState(ext_sec, ext_key, 0)
+r.SetExtState(ext_sec, ext_key, view, 0)
+
+r.Undo_BeginBlock() r.PreventUIRefresh(1)
+
+r.PreventUIRefresh(-1) r.Undo_EndBlock('Save MIDI editor view', 2)
+
