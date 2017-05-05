8fae4df1ba616fc92de41fce2250dc6c011983d0
me2beats <prod.me2beats@gmail.com>
2017-05-05 07:01:13 +0500

    +

--- MIDI Editor/me2beats_Split notes (select right).lua
+++ MIDI Editor/me2beats_Split notes (select right).lua
@@ -1,5 +1,5 @@
 -- @description Split notes (select right)
--- @version 1.0
+-- @version 1.1
 -- @author me2beats
 -- @changelog
 --  + init
@@ -12,18 +12,26 @@ if not take then bla() return end
 local _, notes = r.MIDI_CountEvts(take)
 if notes == 0 then bla() return end
 
-local cur_ppq = r.MIDI_GetPPQPosFromProjTime(take, r.GetCursorPosition())
+local cur_ppq = math.floor(r.MIDI_GetPPQPosFromProjTime(take, r.GetCursorPosition())+0.5)
 
 r.Undo_BeginBlock() r.PreventUIRefresh(1)
+
+local correct
+if r.GetToggleCommandStateEx(32060, 40681) == 1 then
+  r.MIDIEditor_LastFocused_OnCommand(40681,0)--Options: Correct overlapping notes while editing
+  correct = 1
+end
 
 for i = notes-1,0,-1 do
   local _, sel, muted, start_note, end_note, chan, pitch, vel = r.MIDI_GetNote(take, i)
   if sel then
-    if start_note < cur_ppq and end_note > cur_ppq then
+    if start_note < cur_ppq and end_note > cur_ppq then      
       r.MIDI_SetNote(take, i, 0, nil,nil,cur_ppq,nil,nil,nil)
       r.MIDI_InsertNote(take, sel, muted, cur_ppq, end_note, chan, pitch, vel, 0)
     end
   end
 end
+
+if correct then r.MIDIEditor_LastFocused_OnCommand(40681,0) end--Options: Correct overlapping notes while editing
 
 r.PreventUIRefresh(-1) r.Undo_EndBlock('Split notes (select right)', -1)
