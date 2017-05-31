78f1479d227eaa6e578a11cd1ba48b21ae0647f8
me2beats <prod.me2beats@gmail.com>
2017-05-31 13:20:27 +0500

    +

--- Markers and regions/me2beats_Set time selection to nearest project or tempo markers from cursor.lua
+++ Markers and regions/me2beats_Set time selection to nearest project or tempo markers from cursor.lua
@@ -1,32 +1,39 @@
 -- @description Set time selection to nearest project or tempo markers from cursor
--- @version 1.03
+-- @version 1.05
 -- @author me2beats
 -- @changelog
 --  + init
 
 local r = reaper; local function nothing() end; local function bla() r.defer(nothing) end
 
-local cur = r.GetCursorPosition()
+local t_start,t_end,m_start,m_end,m_start_i,t_start_i,cur,x,y
 
-local m_start_i = r.GetLastMarkerAndCurRegion(0, cur)
-local _,_, m_start = r.EnumProjectMarkers(m_start_i)
-local _,_, m_end = r.EnumProjectMarkers(m_start_i+1)
-if m_end<m_start then m_end = nil end
+cur = r.GetCursorPosition()
 
-local t_start_i = r.FindTempoTimeSigMarker(0, cur)
-local t_start,t_end
+m_start_i, t_start_i = r.GetLastMarkerAndCurRegion(0, cur), r.FindTempoTimeSigMarker(0, cur)
+
+if t_start_i ~= -1 then
+  _,_, m_start = r.EnumProjectMarkers(m_start_i)
+  _,_, m_end = r.EnumProjectMarkers(m_start_i+1)
+  if m_end<m_start then m_end = nil end
+end
 
 if t_start_i ~= -1 then
   _, t_start = r.GetTempoTimeSigMarker(0, t_start_i)
   _, t_end = r.GetTempoTimeSigMarker(0, t_start_i+1)
-  if t_end<t_start then t_end = nil end
+  if t_end<=t_start then t_end = nil end
 end
-if not (m_end or t_end) then bla() return end
-local x,y
-if t_start then
+
+if not ((m_start or t_start) and (m_end or t_end)) then bla() return end
+
+if t_start and m_start then
   if cur-m_start < cur-t_start then x = m_start else x = t_start end
-  if m_end and m_end-cur < t_end-cur then y = m_end else y = t_end end
-else x = m_start y = m_end end
+elseif t_start then x = t_start else x = m_start end
+
+if t_end and m_end then
+  if m_end-cur < t_end-cur then y = m_end else y = t_end end
+elseif t_end then y = t_end else y = m_end end
+
 if not (x or y) or x == y then bla() return end
 
 r.Undo_BeginBlock() r.PreventUIRefresh(1)
