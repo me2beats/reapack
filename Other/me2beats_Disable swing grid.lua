a1103f6b25003eecbf19313977b9de6eecc750bd
me2beats <prod.me2beats@gmail.com>
2017-09-18 21:00:37 +0500

    +

---
+++ Other/me2beats_Disable swing grid.lua
@@ -0,0 +1,20 @@
+-- @description Disable swing grid
+-- @version 1.0
+-- @author me2beats
+-- @changelog
+--  + init
+
+local r = reaper
+
+function ToggleActionOnOff(id,on,undo_name,undo_flag)
+
+  local state = r.GetToggleCommandState(id)
+  if on == 1 and state == 0 or on == 0 and state ~= 0 then
+    r.Undo_BeginBlock()
+    r.Main_OnCommand(id,0)
+    r.Undo_EndBlock(undo_name, undo_flag)
+  end
+end
+
+
+ToggleActionOnOff(42304,0,'Disable swing grid',-1)
