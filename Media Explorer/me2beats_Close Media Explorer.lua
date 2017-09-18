e7868decf9b16f3783b7719523e3f411511014cd
me2beats <prod.me2beats@gmail.com>
2017-09-18 11:18:39 +0500

    +

--- Media Explorer/me2beats_Close Media Explorer.lua
+++ Media Explorer/me2beats_Close Media Explorer.lua
@@ -1,5 +1,5 @@
 -- @description Close Media Explorer
--- @version 1.0
+-- @version 1.1
 -- @author me2beats
 -- @changelog
 --  + init
@@ -13,7 +13,7 @@ function ToggleActionOnOff(id,on,undo_name,undo_flag)
   local state = r.GetToggleCommandState(id)
   if on == 1 and state == 0 or on == 0 and state ~= 0 then
     r.Undo_BeginBlock()
-    r.Main_OnCommand(r.NamedCommandLookup('_RS4abc20bf6322c03a05eef9c3b3a08ecbdecdff58'),0)
+    r.Main_OnCommand(id,0)
     r.Undo_EndBlock(undo_name, undo_flag)
   end
 end
