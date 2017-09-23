73861153ebd6c664039c3ee08897091b6c2825d1
me2beats <prod.me2beats@gmail.com>
2017-09-23 15:17:05 +0500

    +

---
+++ Transport and navigation/Go to measure 8.lua
@@ -0,0 +1,15 @@
+--------------user area-----------------------------
+local msr = 8
+local moveview = 0 -- set 1 if you want to move view
+----------------------------------------------------
+
+local r = reaper
+
+local time = r.TimeMap_GetMeasureInfo(0, msr)
+
+local play_st = r.GetPlayState()
+if (play_st == 0 or play_st == 2) and r.GetCursorPosition() == time then return end
+
+r.Undo_BeginBlock()
+r.SetEditCurPos2(0, time, moveview, 1)
+r.Undo_EndBlock('Go to measure'..' '..msr, 2)
---
+++ Transport and navigation/Go to measure.lua
@@ -0,0 +1,17 @@
+local r = reaper
+
+local retval, msr = r.GetUserInputs("Go to measure", 1, "Go to measure:", "")
+if not retval then return end
+msr = tonumber(msr)
+if not msr or msr < 0 then return end
+
+msr = math.floor(msr+.5)
+
+local time = r.TimeMap_GetMeasureInfo(0, msr)
+
+local play_st = r.GetPlayState()
+if (play_st == 0 or play_st == 2) and r.GetCursorPosition() == time then return end
+
+r.Undo_BeginBlock()
+r.SetEditCurPos2(0, time, 0, 1)
+r.Undo_EndBlock('Go to measure'..' '..msr, 2)
